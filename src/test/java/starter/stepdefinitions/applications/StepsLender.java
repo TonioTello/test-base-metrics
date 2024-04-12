017b72e28c6889c8122a0694c271edcd265e3c13
Neyffer Palacios <tonyffer7@hotmail.com>
2024-04-12 12:36:53 +0200

    update from personal pc - make leander pass

--- src/test/java/starter/stepdefinitions/applications/StepsLender.java
+++ src/test/java/starter/stepdefinitions/applications/StepsLender.java
@@ -64,7 +64,7 @@ public class StepsLender {
 
     @And("the lender see that an application has been created")
     public void theLenderSeeThatAnApplicationHasBeenCreated() {
-        //throw new io.cucumber.java.PendingException();
+        ///throw new io.cucumber.java.PendingException();
         //Assert.fail();
     }
 
